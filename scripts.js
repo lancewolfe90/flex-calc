641c818d63e5e7cfffdc73cc4122b0665ecde6f9
Lance Wolfe <lwolfe@talentpath.com>
2021-12-17 14:40:10 -0600

    A little more JavaScript

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
+const outputNode = document.getElementsByClassName('output-row')[0];
 const blankBtn = document.getElementById('blank-btn');
+const equalsBtn = document.getElementById('equals-btn');
+const clearBtn = document.getElementById('clear-btn');
 
 function showMessage() {
     alert('Nope--that button doesn\'t do anything.');
 }
 
+function writeText() {
+    outputNode.innerHTML = '<span>Calculator doesn\'t actually function...</span>'
+}
+
+function clearText() {
+    outputNode.innerHTML = '';
+}
+
 blankBtn.addEventListener('click', showMessage);
+equalsBtn.addEventListener('click', writeText);
+clearBtn.addEventListener('click', clearText);
